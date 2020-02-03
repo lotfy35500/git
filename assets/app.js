7d0f7792a721a3d5305e91a1446ef830e164f5d3
DAH 05 <dah05@FORMATION.lan>
2020-02-03 10:37:23 +0100

    animateText

--- assets/app.js
+++ assets/app.js
@@ -2,5 +2,17 @@ var windows = this;
 
 let h1 = document.querySelector('h1');
 if(h1) {
-  
+  let text = h1.innerHTML;
+  animateText(h1, text);
+}
+
+function animateText(el, text) {
+  el.innerHTML = '';
+  for (var i = 0; i< text.length; i++) {
+    const lettre = text[i]
+    setTimeout(function() {
+      el.innerHTML += lettre;
+
+    },i*300);
+  }
 }
